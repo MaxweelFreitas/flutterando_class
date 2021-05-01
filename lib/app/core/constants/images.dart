269e7a8c3e6bf788cf3bb7feec40cfb8f58912b0
ygort <ygor.teles@access.run>
2021-05-01 13:49:51 -0300

    criado o  arquivo de constants para os assets de imagem

---
+++ lib/app/core/constants/images.dart
@@ -0,0 +1,31 @@
+//--------------------------------------------------------------------//
+//------------------------------Images--------------------------------//
+//--------------------------------------------------------------------//
+
+class AppImages {
+  static const String arquiteturaLimpa = 'assets/books/arquitetura_limpa.png';
+  static const String codigoLimpo = 'assets/books/codigo_limpo.png';
+  static const String dart = 'assets/books/dart.png';
+  static const String flutterForDummies = 'assets/books/flutter_for_dummies.png';
+  static const String flutterFramework = 'assets/books/flutter_framework.png';
+  static const String flutterNaPratica = 'assets/books/flutter_na_pratica.png';
+  static const String flutterProjects = 'assets/books/flutter_projects.png';
+  static const String logo = 'assets/logo/logo.png';
+  static const String logoWhite = 'assets/logo/logo_white.png';
+
+  //--------------------------------------------------------------------//
+  //------------------------------Images Svg----------------------------//
+  //--------------------------------------------------------------------//
+  static const String arrowDown = 'assets/svgs/arrowDown.svg';
+  static const String arrowLeft = 'assets/svgs/arrowLeft.svg';
+  static const String arrowRight = 'assets/svgs/arrowRight.svg';
+  static const String arrowUp = 'assets/svgs/arrowUp.svg';
+  static const String card = 'assets/svgs/card.svg';
+  static const String circle = 'assets/svgs/circle.svg';
+  static const String classes = 'assets/svgs/classes.svg';
+  static const String flagBr = 'assets/svgs/flagBr.svg';
+  static const String rescue = 'assets/svgs/rescue.svg';
+  static const String stop = 'assets/svgs/stop.svg';
+  static const String trails = 'assets/svgs/trails.svg';
+  static const String user = 'assets/svgs/user.svg';
+}
