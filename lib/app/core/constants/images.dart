104c425a570863bef3c907567de21caa3906324f
ygort <ygor.teles@access.run>
2021-05-01 13:55:47 -0300

    ajuste no path dos assets

--- lib/app/core/constants/images.dart
+++ lib/app/core/constants/images.dart
@@ -3,29 +3,29 @@
 //--------------------------------------------------------------------//
 
 class AppImages {
-  static const String arquiteturaLimpa = 'assets/books/arquitetura_limpa.png';
-  static const String codigoLimpo = 'assets/books/codigo_limpo.png';
-  static const String dart = 'assets/books/dart.png';
-  static const String flutterForDummies = 'assets/books/flutter_for_dummies.png';
-  static const String flutterFramework = 'assets/books/flutter_framework.png';
-  static const String flutterNaPratica = 'assets/books/flutter_na_pratica.png';
-  static const String flutterProjects = 'assets/books/flutter_projects.png';
-  static const String logo = 'assets/logo/logo.png';
-  static const String logoWhite = 'assets/logo/logo_white.png';
+  static const String arquiteturaLimpa = 'assets/img/books/arquitetura_limpa.png';
+  static const String codigoLimpo = 'assets/img/books/codigo_limpo.png';
+  static const String dart = 'assets/img/books/dart.png';
+  static const String flutterForDummies = 'assets/img/books/flutter_for_dummies.png';
+  static const String flutterFramework = 'assets/img/books/flutter_framework.png';
+  static const String flutterNaPratica = 'assets/img/books/flutter_na_pratica.png';
+  static const String flutterProjects = 'assets/img/books/flutter_projects.png';
+  static const String logo = 'assets/img/logo/logo.png';
+  static const String logoWhite = 'assets/img/logo/logo_white.png';
 
   //--------------------------------------------------------------------//
   //------------------------------Images Svg----------------------------//
   //--------------------------------------------------------------------//
-  static const String arrowDown = 'assets/svgs/arrowDown.svg';
-  static const String arrowLeft = 'assets/svgs/arrowLeft.svg';
-  static const String arrowRight = 'assets/svgs/arrowRight.svg';
-  static const String arrowUp = 'assets/svgs/arrowUp.svg';
-  static const String card = 'assets/svgs/card.svg';
-  static const String circle = 'assets/svgs/circle.svg';
-  static const String classes = 'assets/svgs/classes.svg';
-  static const String flagBr = 'assets/svgs/flagBr.svg';
-  static const String rescue = 'assets/svgs/rescue.svg';
-  static const String stop = 'assets/svgs/stop.svg';
-  static const String trails = 'assets/svgs/trails.svg';
-  static const String user = 'assets/svgs/user.svg';
+  static const String arrowDown = 'assets/img/svgs/arrowDown.svg';
+  static const String arrowLeft = 'assets/img/svgs/arrowLeft.svg';
+  static const String arrowRight = 'assets/img/svgs/arrowRight.svg';
+  static const String arrowUp = 'assets/img/svgs/arrowUp.svg';
+  static const String card = 'assets/img/svgs/card.svg';
+  static const String circle = 'assets/img/svgs/circle.svg';
+  static const String classes = 'assets/img/svgs/classes.svg';
+  static const String flagBr = 'assets/img/svgs/flagBr.svg';
+  static const String rescue = 'assets/img/svgs/rescue.svg';
+  static const String stop = 'assets/img/svgs/stop.svg';
+  static const String trails = 'assets/img/svgs/trails.svg';
+  static const String user = 'assets/img/svgs/user.svg';
 }
