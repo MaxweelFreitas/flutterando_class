e83541cbd773213c8c8392cdc92c0317024d69de
Maxweel Freitas <maxweel.robot@outlook.com>
2021-05-01 12:24:37 -0300

    second commit

---
+++ lib/app/core/constants/colors.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+//--------------------------------------------------------------------//
+//------------------------------Colors--------------------------------//
+//--------------------------------------------------------------------//
+
+class AppColors {
+  static const Color primary = Color(0xffFFFFFF);
+  static const Color accent = Color(0xffE7343F);
+  static const Color keyboarBar = Color(0xffE8EAED);
+  static const Color banner = Color(0xffCC1B26);
+  static const Color confirmation = Color(0xff43CDA7);
+  static const Color mainText = Color(0xff000000);
+  static const Color hint = Color(0xff656A6E);
+  static const Color confirmationIcon = Color(0xff43CDA7);
+  static const Color radioLabel = Color(0xff8D8F90);
+  static const Color unselectedIcon = Color(0xff828282);
+  static const Color buttonLabel = Color(0xffB3B5B6);
+  static const Color unselectedButton = Color(0xffECEDEE);
+  static const Color points = Color(0xff4F4F4F);
+  static const Color bottomSheetRetangle = Color(0xffC4C4C4);
+}
