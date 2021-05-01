ca88a1be2596151df7860bfd4fdff6ddaf2bc483
ygort <ygor.teles@access.run>
2021-05-01 13:20:37 -0300

    criado o  arquivo de constants de cores

--- lib/app/core/constants/colors.dart
+++ lib/app/core/constants/colors.dart
@@ -7,18 +7,15 @@ import 'package:flutter/material.dart';
 //--------------------------------------------------------------------//
 
 class AppColors {
-  static const Color primary = Color(0xffFFFFFF);
-  static const Color accent = Color(0xffE7343F);
-  static const Color keyboarBar = Color(0xffE8EAED);
-  static const Color banner = Color(0xffCC1B26);
-  static const Color confirmation = Color(0xff43CDA7);
-  static const Color mainText = Color(0xff000000);
-  static const Color hint = Color(0xff656A6E);
-  static const Color confirmationIcon = Color(0xff43CDA7);
-  static const Color radioLabel = Color(0xff8D8F90);
-  static const Color unselectedIcon = Color(0xff828282);
-  static const Color buttonLabel = Color(0xffB3B5B6);
-  static const Color unselectedButton = Color(0xffECEDEE);
-  static const Color points = Color(0xff4F4F4F);
-  static const Color bottomSheetRetangle = Color(0xffC4C4C4);
+  static const Color primary = Color(0xff000000);
+  static const Color accent = Color(0xff00E7F9);
+  static const Color secondary = Color(0xffFFFFFF);
+  static const Color cardBackground = Color(0xff202227);
+  static const Color title = secondary;
+  static const Color subtitle = Color(0xff86888D);
+  static const Color bgProgressbar = Color(0xff86888D);
+  static const Color progressBar = secondary;
+  static const Color progressBarDone = accent;
+  static const Color selectedTabBar = Color(0xff64666B);
+  static const Color unselectedTabBar = cardBackground;
 }
